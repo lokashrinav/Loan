5df309fd6dbd25a5dbd9bc0407b1324e649060fc
Paradox560 <65742681+Paradox560@users.noreply.github.com>
2024-04-21 03:37:27 -0400

    made changes to HomePage and merged changes

--- LoginScreen.js
+++ LoginScreen.js
@@ -44,33 +44,16 @@ export default function LoginScreen() {
   return (
     <SafeAreaView style={styles.container}>
         
-        <Text style={styles.title}>Login</Text>
-        <View style={styles.inputView}>
-            <TextInput style={styles.input} placeholder='EMAIL OR USERNAME' value={username} onChangeText={setUsername} autoCorrect={false}
-        autoCapitalize='none' />
-            <TextInput style={styles.input} placeholder='PASSWORD' secureTextEntry value={password} onChangeText={setPassword} autoCorrect={false}
-        autoCapitalize='none'/>
-        </View>
-        <View style={styles.rememberView}>
-            <View style={styles.switch}>
-                <Switch  value={click} onValueChange={setClick} trackColor={{true : "green" , false : "gray"}} />
-                <Text style={styles.rememberText}>Remember Me</Text>
-            </View>
-            <View>
-                <Pressable onPress={() => Alert.alert("Forget Password!")}>
-                    <Text style={styles.forgetText}>Forgot Password?</Text>
-                </Pressable>
-            </View>
-        </View>
-
+        <Text style={styles.title}>Welcome</Text>
+      
         <View style={styles.buttonView}>
-            <Pressable style={styles.button} onPress={() => Alert.alert("Login Successfuly!","see you in my instagram if you have questions : must_ait6")}>
-                <Text style={styles.buttonText}>LOGIN</Text>
-            </Pressable>
+            <Button disabled={!request} title="Login" onPress={() => promptAsync()} />
             <Text style={styles.optionsText}>OR LOGIN WITH</Text>
         </View>
 
-        <Text style={styles.footerText}>Don't Have Account?<Text style={styles.signup}>  Sign Up</Text></Text>
+        <View style={styles.buttonView}>
+            <Button disabled={!request} title="Sign Up" onPress={() => promptAsync()} />
+        </View>
 
         
     </SafeAreaView>
